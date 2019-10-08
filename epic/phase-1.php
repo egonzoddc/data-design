cc34ddbfc23444ca148116f14f802d893b292a83
Efren Gonzales <colorado3106@gmail.com>
2019-10-08 13:48:25 -0600

    my data design project

--- epic/phase-1.php
+++ epic/phase-1.php
@@ -5,25 +5,25 @@
 	<title>Efren Data Design Project</title>
 </head>
 <body>
-<div>
+<div align="center">
 		<img src="images/broncos.png"style="width:300px;height:200px;">
 		<img src="images/nfl-logo.png">
 		<img src="images/raiders.jpg"style="width:250px;height:250px;">
 </div>
-		<h><strong>Persona:</strong></h>
-			<p>People of all ages and abilities who would like to keep up with the Stats and News of their favorite NFL team at anytime and anywhere using any and all devices.</p>
-		<h><strong>User Story:</strong></h>
-			<p>As a user of this site I would like to have the ability to check on my team to see who we have played, who we play next, and what all the stats of the players of my team are.</p>
-		<h><strong>Use Case:</strong></h>
-		<p>John and Tom are at the bar arguing over whos team is better. John is a Raiders fan, and Tom is a Broncos fan. They are going back and forth over whos team has the better stats. As any NFL fan would know, the Broncos and Raiders rivalry goes back decades.
+		<h1 align="center"><strong></strong><font size="6">Persona:</font></h1>
+			<p align="center">People of all ages and abilities who would like to keep up with the Stats and News of their favorite NFL team at anytime and anywhere using any and all devices.</p>
+		<h2 align="center"><strong></strong><font size="6">User Story:</font></h2>
+			<p align="center">As a user of this site I would like to have the ability to check on my team to see who we have played, who we play next, and what all the stats of the players of my team are.</p>
+		<h3 align="center"><strong></strong><font size="6">Use Case:</font></h3>
+		<p align="center">John and Tom are at the bar arguing over whos team is better. John is a Raiders fan, and Tom is a Broncos fan. They are going back and forth over whos team has the better stats. As any NFL fan would know, the Broncos and Raiders rivalry goes back decades.
 		John wants to show Tom that the Raiders are actually doing better then the Broncos even though the season is 5 weeks old. </p>
-		<h><strong>Interaction Flow:</strong></h>
-			<ul>John takes out his phone and opens the web browser</ul>
-			<ul>John enters NFL.com into his web browser</ul>
-			<ul>Webstie returns with multiple link options to look at including the standings and records of each team</ul>
-			<ul>John clicks on the Link that says "Standings"</ul>
-			<ul>Standings link returns the win-loss record for all the teams in the League</ul>
-			<ul>John clicks on the "Raiders" link</ul>
-			<ul>Raiders Link returns the Win-Loss Record of the team and who they played and the perspective scores of each game</ul>
+		<h4 align="center"><strong></strong><font size="6">Interaction Flow:</font></h4>
+			<ul align="center">John takes out his phone and opens the web browser</ul>
+			<ul align="center">John enters NFL.com into his web browser</ul>
+			<ul align="center">Webstie returns with multiple link options to look at including the standings and records of each team</ul>
+			<ul align="center">John clicks on the Link that says "Standings"</ul>
+			<ul align="center">Standings link returns the win-loss record for all the teams in the League</ul>
+			<ul align="center">John clicks on the "Raiders" link</ul>
+			<ul align="center">Raiders Link returns the Win-Loss Record of the team and who they played and the perspective scores of each game</ul>
 </body>
 </html>
