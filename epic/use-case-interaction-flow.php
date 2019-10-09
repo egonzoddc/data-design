173e71f2be6abf9965779adacb60dde84d1b7469
Efren Gonzales <colorado3106@gmail.com>
2019-10-09 15:35:47 -0600

    my data design project broncos vs. raiders

--- epic/use-case-interaction-flow.php
+++ epic/use-case-interaction-flow.php
@@ -10,17 +10,23 @@
 	<img src="images/nfl-logo.png">
 	<img src="images/raiders.jpg"style="width:250px;height:250px;">
 </div>
-<h1 align="center"><strong></strong><font size="6">Use Case/Interaction Flow:</font></h1>
-<p align="center">John and Tom are at the bar arguing over who's team is has won more games since they started playing each other.
-	John, is a Raiders fan. Tom, is a Broncos fan. The Broncos and Raiders rivalry goes back decades.
-	Tom wants to show John that the Broncos have won more games then the Raiders since they started playing each other in 1960.</p>
-<h2 align="center"><strong></strong><font size="6">Interaction Flow:</font></h2>
-<ul align="center">John takes out his phone and opens the web browser</ul>
-<ul align="center">John enters NFL.com into his web browser</ul>
-<ul align="center">Webstie returns with multiple link options to look at including the standings and records of each team</ul>
-<ul align="center">John clicks on the Link that says "Standings"</ul>
-<ul align="center">Standings link returns the win-loss record for all the teams in the League</ul>
-<ul align="center">John clicks on the "Raiders" link</ul>
-<ul align="center">Raiders Link returns the Win-Loss Record of the team and who they played and the perspective scores of each game</ul>
+	<h1 align="center"><strong></strong><font size="6">Use Case/Interaction Flow:</font></h1>
+		<p align="center">John and Tom are at the bar arguing over who's team is has won more games since they started playing each other.
+		John, is a Raiders fan. Tom, is a Broncos fan. The Broncos and Raiders rivalry goes back decades.
+		Tom wants to show John that the Broncos have won more games then the Raiders since they started playing each other in 1960.</p>
+	<h2 align="center"><strong></strong><font size="6">Interaction Flow:</font></h2>
+		<ul align="center">Tom takes out his phone and opens the web browser</ul>
+		<ul align="center">Tom enters NFL.com into his web browser</ul>
+		<ul align="center">Webstie returns with multiple link options to look at including news, schedule, standings, and records of each team </ul>
+		<ul align="center">Tom clicks on the Link that says "Schedule"</ul>
+		<ul align="center">Games link returns links for every team in the League</ul>
+		<ul align="center">Tom clicks on the "Broncos" link</ul>
+		<ul align="center">The link returns the the games the Broncos have played and who they will be playing </ul>
+		<ul align="center">Tom clicks on the "Broncos vs. Raiders" link. They played in Week 1 so it is right up top</ul>
+		<ul align="center">The link returns the last time they played and the next time they play this season along with a "stats" link</ul>
+		<ul align="center">Tom clicks on the "stats" link</ul>
+		<ul align="center">Stats link returns all stats of the last game they played and an option for "History" stats</ul>
+		<ul align="center">Tom clicks on the "History" link revealing the fact that the teams have played a total of 120 games since 1960. Also showing
+									that out of those 120 games the Raiders have won a total of 65 and the Broncos a total of 53 with two games ending in a tie score</ul>
 </body>
 </html>
